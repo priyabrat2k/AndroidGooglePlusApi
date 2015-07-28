73339bf83c750b692970d75edaff2bbda23ca8dc
Priyabrat <ppadhy@regalix-inc.com>
2015-07-28 18:20:16 +0530

    tvh

--- app/src/main/java/com/appifiedtech/androidgoogleplusapi/MainActivity.java
+++ app/src/main/java/com/appifiedtech/androidgoogleplusapi/MainActivity.java
@@ -1,18 +1,60 @@
 package com.appifiedtech.androidgoogleplusapi;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 
 
-public class MainActivity extends AppCompatActivity {
-    //https://developers.google.com/identity/sign-in/android/start-integrating
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final int REQUEST_CODE = 0;
+    private String TAG = getClass().getName();
+    private GoogleApiClient googleApiClient = null;
+    private SignInButton signInButton;
+    private boolean isResolving = false;
+    private boolean shouldResolve = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
     }
 
     @Override
@@ -35,5 +77,81 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "user is now connected");
+        if (Plus.PeopleApi.getCurrentPerson(googleApiClient) != null) {
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(googleApiClient);
+            String personName = currentPerson.getDisplayName();
+            String personGooglePlusProfile = currentPerson.getUrl();
+            Log.d(TAG, "Profile Info " + personName + " " + personGooglePlusProfile);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "user is now suspended connection");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection Failed " + connectionResult.toString());
+        if (!isResolving && shouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(MainActivity.this, REQUEST_CODE);
+                    isResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.d(TAG, "Exception due to " + e);
+                    isResolving = false;
+                    googleApiClient.connect();
+                }
+            } else {
+                Log.d(TAG, "Error Message, can't connect");
+            }
+        } else {
+            Log.d(TAG, "Show sign out UI");
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.sign_in_button) {
+            makeSignIn();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == REQUEST_CODE) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                shouldResolve = false;
+            }
+
+            isResolving = false;
+            googleApiClient.connect();
+        }
+    }
+
+    private void makeSignIn() {
+        shouldResolve = true;
+        googleApiClient.connect();
+        Toast.makeText(getApplicationContext(), "Signining in", Toast.LENGTH_LONG).show();
+    }
+
+    private void makeSignUp() {
+        if (googleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(googleApiClient);
+            googleApiClient.disconnect();
+        }
+        Toast.makeText(getApplicationContext(), "Signing out", Toast.LENGTH_LONG).show();
     }
 }
